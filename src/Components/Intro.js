855d38797789cf17fbe689e7c3740be64209b541
Zain Ul Din <zain.personal47@gmail.com>
2022-04-24 17:52:50 +0500

    firebase img added

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -5,11 +5,12 @@ const IMG_PATH = 'bgIntro.jpg'
 
 // <img className ='introImg img-responsive' src={`../static/${IMG_PATH}`} placeholder='Error' alt='loading...'/> 
 
+// https://raw.githubusercontent.com/Zain-ul-din/smart_ESite/master/public/Static/bgIntro.jpg
 export default function Intro (props) {
     return (
         <>
            <div className='Container'>
-              <img className ='introImg img-responsive' src={`require(/Static/${IMG_PATH})`} placeholder='Error' alt='loading...'/> 
+              <img className ='introImg img-responsive' src={`https://firebasestorage.googleapis.com/v0/b/ecomappi.appspot.com/o/files%2FbgIntro.jpg?alt=media&token=cd7d28b4-a7d0-44b6-9afa-d11ece3fc828`} placeholder='Error' alt='loading...'/> 
               <div className="centeredTop p-2"><h1 className='text-light'>Welcome to SPS SULTAN PIPE STORE SITE</h1></div>
               
               <div className="centered hideUnderMin p-2">
