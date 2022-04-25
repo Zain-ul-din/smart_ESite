dd0b9a68321fa03966d1f18d9d2cca047c8c31f0
Zain Ul Din <zain.personal47@gmail.com>
2022-04-26 00:07:48 +0500

    google vef

--- src/Env.js
+++ src/Env.js
@@ -1,7 +1,6 @@
 
 
+
 export const API_KEY = "AIzaSyDQ85KC0g2YZee5x6NFAdViEIrsCzdolyk" 
-
 export const ADMIN_MAIL = "zain.personal47@gmail.com"
-
 export const USER_MAIL = "sultanpipes123@gmail.com"
